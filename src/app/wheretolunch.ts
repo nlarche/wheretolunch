15080e6b766a974aad120d8a6dc4ac8b19679e9b
angular-cli <angular-cli@angular.io>
2015-11-17 13:32:32 +0100

    chore: initial commit from angular-cli v0.0.0

---
+++ src/app/wheretolunch.ts
@@ -0,0 +1,15 @@
+import {Component, bootstrap} from 'angular2/angular2';
+
+
+@Component({
+  selector: 'wheretolunch-app',
+  providers: [],
+  templateUrl: 'app/wheretolunch.html',
+  directives: [],
+  pipes: []
+})
+export class WheretolunchApp {
+
+  constructor() {}
+
+}
