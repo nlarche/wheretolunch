3be8a3d19c6496a3819f3ce2cc5690ffc5718592
nilar <nicolas.larche@rca.fr>
2015-11-18 14:18:09 +0100

    "add new feature"

--- src/app/wheretolunch.ts
+++ src/app/wheretolunch.ts
@@ -1,4 +1,22 @@
-import {Component, bootstrap} from 'angular2/angular2';
+import {Component} from 'angular2/angular2';
+
+class Place {
+  id: number;
+  name : string  
+}
+
+var PLACES: Place[] = [
+  { "id": 11, "name": "Mr. Nice" },
+  { "id": 12, "name": "Narco" },
+  { "id": 13, "name": "Bombasto" },
+  { "id": 14, "name": "Celeritas" },
+  { "id": 15, "name": "Magneta" },
+  { "id": 16, "name": "RubberMan" },
+  { "id": 17, "name": "Dynama" },
+  { "id": 18, "name": "Dr IQ" },
+  { "id": 19, "name": "Magma" },
+  { "id": 20, "name": "Tornado" }
+];
 
 
 @Component({
@@ -7,9 +25,25 @@ import {Component, bootstrap} from 'angular2/angular2';
   templateUrl: 'app/wheretolunch.html',
   directives: [],
   pipes: []
+
 })
 export class WheretolunchApp {
-
-  constructor() {}
-
+  newPlace : Place;
+  places : Place[];   
+  
+  addPlace(place: Place){
+   
+    if (place.name){
+   
+      console.log(place);
+      this.places.push(place);
+      this.newPlace.name = null;  
+   
+   }    
+  };
+    
+  constructor() {
+    this.newPlace = new Place();    
+    this.places = PLACES;
+  }
 }
